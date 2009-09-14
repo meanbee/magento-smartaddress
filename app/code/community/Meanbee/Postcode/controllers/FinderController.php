4f08f6c45f0bef5d5e7401ed883db83414ce3f4c
nrj <nrj@9d90f426-aa7f-4aa9-92b5-05b901949259>
2009-09-14 19:20:58 +0000

    Add some error checking on /postcode/finder/single

--- app/code/community/Meanbee/Postcode/controllers/FinderController.php
+++ app/code/community/Meanbee/Postcode/controllers/FinderController.php
@@ -12,60 +12,67 @@
  * @license    Single Site License, requiring consent from Meanbee Internet Solutions
  */
 class Meanbee_Postcode_FinderController extends Mage_Core_Controller_Front_Action {
-	public function preDispatch() {
-		if (Mage::getStoreConfig('postcode/options/security') ) {
-			if (isset($_SERVER['HTTP_REFERER'])) {				
-				$us = Mage::getUrl();
-				$us = substr($us, strpos($us, ':') + 3);
-				
-				preg_match('/https?:\/\/([a-zA-Z0-9\.]+\/)/i', $_SERVER['HTTP_REFERER'], $matches);
-				if (count($matches) == 2) {
-					$them = $matches[1]; 
-				}
-				
-				//echo $us . " - " . $them;
-				
-				if ($us != $them) {
-					echo Zend_Json::encode(array(
-						"error" => true,
-						"content" => "Security check failed.  Request identified as originating from '$them' need '$us'"
-					));
-					exit;
-				}
-			} else {
-					echo Zend_Json::encode(array(
-						"error" => true,
-						"content" => "Security check failed.  Unable to identify referrer."
-					));
-					exit;
-			}
-		}
-	}
-	
-	public function multipleAction() {
-		header("Content-type: application/json");
+    public function preDispatch() {
+        if (Mage::getStoreConfig('postcode/options/security') ) {
+            if (isset($_SERVER['HTTP_REFERER'])) {                
+                $us = Mage::getUrl();
+                $us = substr($us, strpos($us, ':') + 3);
+                
+                preg_match('/https?:\/\/([a-zA-Z0-9\.]+\/)/i', $_SERVER['HTTP_REFERER'], $matches);
+                if (count($matches) == 2) {
+                    $them = $matches[1]; 
+                }
+                
+                //echo $us . " - " . $them;
+                
+                if ($us != $them) {
+                    echo Zend_Json::encode(array(
+                        "error" => true,
+                        "content" => "Security check failed.  Request identified as originating from '$them' need '$us'"
+                    ));
+                    exit;
+                }
+            } else {
+                    echo Zend_Json::encode(array(
+                        "error" => true,
+                        "content" => "Security check failed.  Unable to identify referrer."
+                    ));
+                    exit;
+            }
+        }
+    }
+    
+    public function multipleAction() {
+        header("Content-type: application/json");
 
-		$postcode = strtolower(preg_replace("/[^a-zA-Z0-9s]/", "", str_replace(' ', '', $_GET['postcode'])));
+        $postcode = strtolower(preg_replace("/[^a-zA-Z0-9s]/", "", str_replace(' ', '', $_GET['postcode'])));
 
-		if (!empty($postcode)) {
-			$call = Mage::getModel('postcode/call');
-			echo $call->findMultipleByPostcode($postcode);
-		} else {
-			echo Zend_Json::encode(array(
-				"error" => true,
-				"content" => "No postcode provided"
-			));
-		}
-		exit;
-	}
+        if (!empty($postcode)) {
+            $call = Mage::getModel('postcode/call');
+            echo $call->findMultipleByPostcode($postcode);
+        } else {
+            echo Zend_Json::encode(array(
+                "error" => true,
+                "content" => "No postcode provided"
+            ));
+        }
+        exit;
+    }
 
-	public function singleAction() {
-		header("Content-type: application/json");
+    public function singleAction() {
+        header("Content-type: application/json");
 
-		$id = (int) $_GET['id'];
+        if (isset($_GET['id'])) {
+            $id = (int) $_GET['id'];
 
-		$call = Mage::getModel('postcode/call');
-		echo $call->findSingleAddressById($id);
-		exit;
-	}
+            $call = Mage::getModel('postcode/call');
+            echo $call->findSingleAddressById($id);
+            exit;
+        } else {
+            echo Zend_Json::encode(array(
+                "error" => true,
+                "content" => "No address ID provided"
+            ));
+        }
+    }
 }
